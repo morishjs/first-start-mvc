90f686f03db7b07d22f377429dc99b5917efa6f2
humonnom <juepark42@gmail.com>
2023-02-11 22:06:46 +0900

    find user data 연동
     - lodash 설치
     - User model update(add phone, age field)
     - findUser 메소드에서 Option 타입 사용

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,21 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { User } from '../user';
 import { UserService } from './user.service';
-
+import * as O from 'fp-ts/Option';
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('/find-user')
   @Render('find-user')
-  findUser(): { user: User } {
-    const user = this.userService.findUser(0);
-    return { user };
+  async findUser(): Promise<{ user: User }> {
+    const user = await this.userService.findUser(0);
+
+    if (O.isSome(user)) {
+      return { user: user.value as User };
+    } else {
+      return { user: new User() };
+    }
   }
 
   //TODO: createUser 생성
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import { User } from '../user';
+import * as O from 'fp-ts/Option';
+import _ from 'lodash';
 
+const prisma = new PrismaClient();
 @Injectable()
 export class UserService {
-  // TODO: 실제로 DB에서 가져오기
-  findUser(id: number): User {
-    const user1 = new User();
-    user1.age = 20;
-    user1.name = 'junsuk';
-    user1.phone = '01087706498';
+  async findUser(id: number): Promise<O.Option<User>> {
+    const data = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        phone: true,
+        age: true,
+        name: true,
+      },
+    });
 
-    return user1;
+    if (data) {
+      return O.some(data as User);
+    }
+
+    return O.none;
   }
 }
